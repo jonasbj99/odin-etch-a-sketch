37d6d3f9320f68fc9cf6a6d25f718ed3f1f6c54e
jonasbj99 <99955044+jonasbj99@users.noreply.github.com>
2026-02-13 21:11:19 +0100

    Add grid clear and rainbow mode

--- js/etchasketch.js
+++ js/etchasketch.js
@@ -3,23 +3,25 @@ const etchContainer = document.querySelector('[class^="etch-container"]');
 const settingsObj = {
   color: "#000",
   size: 16,
+  clickOn: false,
+  rainbowOn: true,
 };
 
-generateGrid(etchContainer, settingsObj.size);
+generateGrid(settingsObj.size);
 
-function generateGrid(container, size) {
+function generateGrid(size) {
   // Clear existing grid
-  const children = container.querySelectorAll("*");
+  const children = etchContainer.querySelectorAll("*");
   children.forEach((child) => child.remove());
 
   // Remove any existing grid size class
-  container.classList.forEach((className) => {
+  etchContainer.classList.forEach((className) => {
     if (className.startsWith("etch-container")) {
-      container.classList.remove(className);
+      etchContainer.classList.remove(className);
     }
   });
 
-  container.classList.add(`etch-container-${size}`);
+  etchContainer.classList.add(`etch-container-${size}`);
 
   // Add new grid children based on size
   for (let i = 1; i <= size; i++) {
@@ -30,15 +32,37 @@ function generateGrid(container, size) {
       const newEl = document.createElement("div");
       newEl.setAttribute("id", `${i}-${j}`);
       newEl.classList.add("etch-pixel");
-      newEl.addEventListener("mouseenter", colorPixel);
+
+      if (settingsObj.clickOn) {
+        newEl.addEventListener("click", colorPixel);
+      } else {
+        newEl.addEventListener("mouseenter", colorPixel);
+      }
       newRow.appendChild(newEl);
     }
 
-    container.appendChild(newRow);
+    etchContainer.appendChild(newRow);
   }
 }
 
 function colorPixel(event) {
+  if (settingsObj.rainbowOn) {
+    const rNum = Math.floor(Math.random() * 255);
+    const gNum = Math.floor(Math.random() * 255);
+    const bNum = Math.floor(Math.random() * 255);
+    settingsObj.color = `rgb(${rNum}, ${gNum}, ${bNum})`;
+  }
   const el = event.currentTarget;
   el.style.backgroundColor = settingsObj.color;
 }
+
+function clearGrid() {
+  const children = etchContainer.querySelectorAll("*");
+  children.forEach((child) => child.removeAttribute("style"));
+}
+
+// Pick color
+
+// Erase
+
+// Opacity mode
